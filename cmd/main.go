7328b610d849c2338636df929e6e812cd966ffe6
Emre Caglar <emrecaglar@gmail.com>
2019-01-04 01:00:42 -0800

    main updated

--- cmd/main.go
+++ cmd/main.go
@@ -7,20 +7,14 @@ import (
 	"net/http"
 )
 
-
-
 func main()  {
 
 	logger  := logger.CreateLogger()
 	db 		:= memstore.CreateInMemDB()
 	server  := server.CreateServer(logger,db)
 
-
 	http.Handle("/",server.Routers)
 	logger.LogInfo("Listening localhost 8080...")
 	http.ListenAndServe("localhost:8080", server.Routers)
-
-
-
 
 }
